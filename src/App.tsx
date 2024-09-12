749afc74299bdbad403316460c57910639c052db
Moulya R <moulya.r@dayforce.com>
2024-09-12 17:58:19 +0530

    rolled back changes via dev in local repo

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         I made some change in github
+        Rollback changes
         </p>
         <a
           className="App-link"
